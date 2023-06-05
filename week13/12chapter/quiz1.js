54f4b3cdf82fea66aafd57116d0caaf5f15b3c50
Gamemode1221 <yongho1221@gmail.com>
2023-06-05 16:23:43 +0900

    13주차

---
+++ week13/12chapter/quiz1.js
@@ -0,0 +1,17 @@
+const URL = "https://reqres.in/api/products/10"
+const result = document.querySelector("#result");
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', URL, true);
+xhr.send();
+
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        let product = JSON.parse(xhr.responseText);
+
+        console.log(product);
+
+        result.innerHTML = `<p>상품명 : ${product.data.name}</p>
+            <p>생산년도 : ${product.data.year}</p>`;
+    }
+}
